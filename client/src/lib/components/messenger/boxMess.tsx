6ca04f09d9a1ef69ee46e58b3547e0782caf44ec
theanh <tranluutheanh17601@gmail.com>
2022-12-17 04:51:16 +0700

    fix

--- client/src/lib/components/messenger/boxMess.tsx
+++ client/src/lib/components/messenger/boxMess.tsx
@@ -20,19 +20,19 @@ import SelectIcon from "../../../assets/icons/select.svg";
 import ClearRoundedIcon from "@mui/icons-material/ClearRounded";
 
 interface IMessage {
-  isuser: boolean;
-  content: string;
-  createdAt: string;
-  userId: string;
-  messageId: string;
+	isuser: boolean;
+	content: string;
+	createdAt: string;
+	userId: string;
+	messageId: string;
 }
 
 interface IBoxMess {
-  user: string;
-  content: string;
-  createdAt: string;
-  userId: string;
-  _id: string;
+	user: string;
+	content: string;
+	createdAt: string;
+	userId: string;
+	_id: string;
 }
 
 type FormValues = {
@@ -97,85 +97,91 @@ const FormChat = () => {
 	);
 };
 
-const ItemMess = ({ isuser, content, createdAt, userId, messageId }: IMessage) => {
-  const { dataFriend } = useDialogInfo(userId);
+const ItemMess = ({
+	isuser,
+	content,
+	createdAt,
+	userId,
+	messageId,
+}: IMessage) => {
+	const { dataFriend } = useDialogInfo(userId);
 
-  const [isHovering, setIsHovering] = useState(false);
+	const [isHovering, setIsHovering] = useState(false);
 
-  const handleMouseOver = () => {
-    setIsHovering(true);
-  };
+	const handleMouseOver = () => {
+		setIsHovering(true);
+	};
 
-  const handleMouseOut = () => {
-    setIsHovering(false);
-  };
+	const handleMouseOut = () => {
+		setIsHovering(false);
+	};
 
-  const {handleDeleteMessage, deleteMessageData} = useBoxChat('');
+	const { handleDeleteMessage, deleteMessageData } = useBoxChat("");
 
-  const deleteMessage = () => {
-    Swal.fire({
-      title: "Do you want to delete this message?",
-      showDenyButton: true,
-      showCancelButton: false,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: `Delete`,
-      cancelButtonText: `Cancel`,
-    }).then((result) => {
-      /* Read more about isConfirmed, isDenied below */
-      if (result.isConfirmed) {
-        handleDeleteMessage(messageId);
-        Swal.fire("Deleted!", "", "success");
-      }
-    });
-  };
+	const deleteMessage = () => {
+		Swal.fire({
+			title: "Do you want to delete this message?",
+			showDenyButton: true,
+			showCancelButton: false,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: `Delete`,
+			cancelButtonText: `Cancel`,
+		}).then((result) => {
+			/* Read more about isConfirmed, isDenied below */
+			if (result.isConfirmed) {
+				handleDeleteMessage(messageId);
+				Swal.fire("Deleted!", "", "success");
+			}
+		});
+	};
 
-  const ItemLeft = () => {
-    return (
-      <div className="flex items-center my-[2px] gap-1 relative md:gap-2  md:my-4">
-        <img
-          src={AvatarDefaultIcon}
-          alt="avatar"
-          className="w-10 h-10 rounded-full md:w-12 md:h-12"
-        />
-        <p className="absolute -top-3 left-16 font-bold text-xs">
-          {dataFriend?.name}
-        </p>
+	const ItemLeft = () => {
+		return (
+			<div className="flex items-center my-[2px] gap-1 relative md:gap-2  md:my-4">
+				<img
+					src={AvatarDefaultIcon}
+					alt="avatar"
+					className="w-10 h-10 rounded-full md:w-12 md:h-12"
+				/>
+				<p className="absolute -top-3 left-16 font-bold text-xs">
+					{dataFriend?.name}
+				</p>
 
-        <div className="px-2 py-1 text-sm bg-primary-wash rounded-xl max-w-[70%] md:px-4 md:py-2 md:text-base md:rounded-3xl">
-          {content}
-        </div>
-      </div>
-    );
-  };
+				<div className="px-2 py-1 text-sm bg-primary-wash rounded-xl max-w-[70%] md:px-4 md:py-2 md:text-base md:rounded-3xl">
+					{content}
+				</div>
+			</div>
+		);
+	};
 
-  const ItemRight = () => {
-    return (
-      <div
-        className="flex items-center gap-1 my-[2px] justify-end md:gap-2 md:my-1"
-        onMouseOut={handleMouseOut}
-        onMouseOver={handleMouseOver}
-      >
-        {isHovering && (
-          <div
-            className="flex items-center justify-center gap-2 cursor-pointer"
-            onClick={deleteMessage}
-            aria-hidden="true"
-          >
-            <ClearRoundedIcon
-              fontSize="small"
-              sx={{
-                color: "#e32124",
-              }}
-            />
-          </div>
-        )}
-        <div className="px-2 py-1 bg-primary text-sm text-white rounded-xl max-w-[70%] md:px-4 md:py-2 md:text-base md:rounded-3xl">
-          {content}
-        </div>
-      </div>
-    );
-  };
+	const ItemRight = () => {
+		return (
+			<div
+				className="flex items-center gap-1 my-[2px] justify-end md:gap-2 md:my-1"
+				onMouseOut={handleMouseOut}
+				onMouseOver={handleMouseOver}
+			>
+				{isHovering && (
+					<div
+						className="flex items-center justify-center gap-2 cursor-pointer"
+						onClick={deleteMessage}
+						aria-hidden="true"
+					>
+						<ClearRoundedIcon
+							fontSize="small"
+							sx={{
+								color: "#e32124",
+							}}
+						/>
+					</div>
+				)}
+				<div className="px-2 py-1 bg-primary text-sm text-white rounded-xl max-w-[70%] md:px-4 md:py-2 md:text-base md:rounded-3xl">
+					{content}
+				</div>
+			</div>
+		);
+	};
 
 	return isuser ? <ItemRight /> : <ItemLeft />;
 };
@@ -247,58 +253,61 @@ const BoxMess = () => {
 			dispatch(toggleInfo());
 		};
 
-    return (
-      <div className="flex justify-between pb-3 border-b md:pb-4">
-        {loadingInfoChannel && <Loading />}
-        <div className="flex gap-2 items-center">
-          {isMobile && (
-            <IconButton onClick={handleBack}>
-              <ChevronLeftRoundedIcon />
-            </IconButton>
-          )}
-          <img
-            src={
-              ListUserOnChannel?.length === 2 && infoParner[0]?.avatar
-                ? infoParner[0]?.avatar
-                : AvatarDefaultIcon
-            }
-            alt="avatar"
-            className="w-10 h-10 rounded-full md:w-14 md:h-14"
-          />
-          <p className="font-bold md:text-xl">
-            {ListUserOnChannel?.length === 2 ? infoParner[0]?.name : infoChannelData?.data?.name}
-          </p>
-        </div>
-        <IconButton>
-          <InfoRoundedIcon
-            classes={{
-              root: "text-primary md:w-10 md:h-10",
-            }}
-            fontSize={isMobile ? "medium" : "large"}
-            onClick={handleClickInfo}
-          />
-        </IconButton>
-      </div>
-    );
-  };
+		return (
+			<div className="flex justify-between pb-3 border-b md:pb-4">
+				{loadingInfoChannel && <Loading />}
+				<div className="flex gap-2 items-center">
+					{isMobile && (
+						<IconButton onClick={handleBack}>
+							<ChevronLeftRoundedIcon />
+						</IconButton>
+					)}
+					<img
+						src={
+							ListUserOnChannel?.length === 2 &&
+							infoParner[0]?.avatar
+								? infoParner[0]?.avatar
+								: AvatarDefaultIcon
+						}
+						alt="avatar"
+						className="w-10 h-10 rounded-full md:w-14 md:h-14"
+					/>
+					<p className="font-bold md:text-xl">
+						{ListUserOnChannel?.length === 2
+							? infoParner[0]?.name
+							: infoChannelData?.data?.name}
+					</p>
+				</div>
+				<IconButton>
+					<InfoRoundedIcon
+						classes={{
+							root: "text-primary md:w-10 md:h-10",
+						}}
+						fontSize={isMobile ? "medium" : "large"}
+						onClick={handleClickInfo}
+					/>
+				</IconButton>
+			</div>
+		);
+	};
 
-  const ChatArea = () => {
-    return (
-      <div className="h-full overflow-y-scroll pt-4">
-        {loadingMessage && <Loading />}
-        {messageData?.data?.map((item: IBoxMess, index: number) => (
-          <ItemMess
-            key={index}
-            isuser={handleIsUser(item?.user)}
-            content={item.content}
-            createdAt={item.createdAt}
-            userId={item.user}
-            messageId={item._id}
-          />
-        ))}
-      </div>
-    );
-  };
+	const ChatArea = () => {
+		return (
+			<div className="h-full overflow-y-auto pt-4 flex flex-col justify-end">
+				{loadingMessage && <Loading />}
+				{messageData?.data?.map((item: IBoxMess, index: number) => (
+					<ItemMess
+						key={index}
+						isuser={handleIsUser(item?.user)}
+						content={item.content}
+						createdAt={item.createdAt}
+						userId={item.user}
+						messageId={item._id}
+					/>
+				))}
+			</div>
+		);
+	};
 
 	const BottomBox = () => {
 		return (
@@ -308,16 +317,16 @@ const BoxMess = () => {
 		);
 	};
 
-  const EmptyScreen = () => {
-    return (
-      <div className="flex flex-col items-center justify-center h-full">
-        <img src={SelectIcon} alt="empty" className="w-[20%] mt-20" />
-        <p className="text-2xl font-bold mt-10">
-          Please select the person to message
-        </p>
-      </div>
-    );
-  };
+	const EmptyScreen = () => {
+		return (
+			<div className="flex flex-col items-center justify-center h-full">
+				<img src={SelectIcon} alt="empty" className="w-[20%] mt-20" />
+				<p className="text-2xl font-bold mt-10">
+					Please select the person to message
+				</p>
+			</div>
+		);
+	};
 
 	return (
 		<div className="bg-white grid grid-rows-[auto,1fr,auto] w-full rounded-xl p-4 h-[calc(100vh-140px)] md:h-[calc(100vh-100px)] md:mr-4">
