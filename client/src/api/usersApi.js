f967be88939c01f87bc7e274b4104df077859a60
theanh <tranluutheanh17601@gmail.com>
2022-12-17 15:14:33 +0700

    update

--- client/src/api/usersApi.js
+++ client/src/api/usersApi.js
@@ -86,3 +86,19 @@ export const GetMySuggestions = async () => {
     }
 }
 
+export const GetNotFriend = async () => {
+    // get the access token from the cookie
+    const access_token = Cookies.get("access_token");
+    // set the authorization header
+    const options = {
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+        },
+    };
+    try {
+        const response = await request.get(`/user/not/friend`, options);
+        return response;
+    } catch (error) {
+        return error?.response;
+    }
+}
